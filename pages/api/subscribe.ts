5e072381c5838a701c4f40c68fbf58bffc529589
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-10-31 18:58:10 +0100

    env variable name fix

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -3,8 +3,8 @@ import mailchimp from '@mailchimp/mailchimp_marketing'
 
 const Subscribe = async(req:NextApiRequest,res:NextApiResponse) => {
     mailchimp.setConfig({
-        apiKey:process.env.MAIL_CHIMP_API_KEY,
-        server:process.env.MAIL_CHIMP_SERVER
+        apiKey:process.env.MAILCHIMP_API_KEY,
+        server:process.env.MAILCHIMP_SERVER
     });
 
     const {email} = req.body;
@@ -13,13 +13,14 @@ const Subscribe = async(req:NextApiRequest,res:NextApiResponse) => {
         res.status(400).json({error:'Email Not Found'})
     }else{
         try {
-            await mailchimp.lists.addListMember(process.env.MAIL_CHIMP_AUDIENCE_ID || '',
+            await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID || '',
                 {
                     email_address:email,
                     status:'subscribed'
                 })
             res.status(200).json({msg:'Email Sent Successfully'})
         } catch (error) {
+            console.log(error)
             res.status(500).json({error:'An Error Occurred'})
         }
     }
