a509a002228d4438c3714e0408eb00189de177de
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-10-31 12:48:18 +0100

    light mode layout fixed

--- Components/Layouts/index.tsx
+++ Components/Layouts/index.tsx
@@ -3,7 +3,7 @@ import {Nav,Footer} from '../index'
 
 const Layout = ({children}:{children:React.ReactElement}) => {
   return(
-    <div className="flex w-full min-h-screen bg-white dark:bg-[#00002B] dark:text-cyan-400 font-['raleway']">
+    <div className="flex w-full min-h-screen bg-white dark:bg-[#00002B] dark:text-cyan-400 text-black font-['raleway']">
       <div className='w-[70%] min-w-[330px] mx-auto'>
         <Nav/>
           {children}
