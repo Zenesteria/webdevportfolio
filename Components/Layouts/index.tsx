5429a797ce603b835b34be04511078263d6afb06
Abdurrahman Aderinto <hexalyne@gmail.com>
2022-11-05 10:03:20 +0100

    animated bg added

--- Components/Layouts/index.tsx
+++ Components/Layouts/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Bg from '../animated-bg/Bg'
 import {Nav,Footer} from '../index'
 
 const Layout = ({children}:{children:React.ReactElement}) => {
   return(
-    <div className="flex w-full min-h-screen bg-white dark:bg-[#00002B] dark:text-cyan-400 text-black font-['raleway']">
-      <div className='w-[70%] min-w-[330px] mx-auto'>
+    <div className="flex relative w-full min-h-screen bg-white dark:bg-[#00002B] dark:text-cyan-400 text-black font-['raleway']">
+      <Bg/>
+      <div className='w-[70%] min-w-[330px] mx-auto z-[20]'>
         <Nav/>
           {children}
         <Footer/>
---
+++ Components/animated-bg/Bg.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+
+export default function Bg() {
+    
+  return (
+    <div className='absolute top-0 right-0 w-full h-full z-[10] overflow-hidden'>
+        <div className="background">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+  )
+}
